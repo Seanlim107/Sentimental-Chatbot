8aa6f7b63e92f3e90fd29e0c9e4a06e2d3b66de6
VICTOR ABIA <transcendingvictor@gmail.com>
2024-03-19 00:02:26 +0100

    Cleaned dataset, tests passed

--- Baseline_chatbot/dataset.py
+++ Baseline_chatbot/dataset.py
@@ -13,38 +13,30 @@ from transformers import PreTrainedTokenizerFast, AutoTokenizer
 # jaxtype pleasseee
 import re
 import sentencepiece as spm
+import ast
 
 
 class MoviePhrasesData(data.Dataset):
-    UNK = "<UNK>"
-    START_TOKEN = "<S>"
-    END_TOKEN = "</S>"
 
     # voc: vocabulary, word:idx
     # all dialogues:
-    def __init__(self, max_seq_len=None, voc_init=False):
+    def __init__(self, max_seq_len=15, voc_init=False):
 
-        custom_header = ["user0", "user1", "movie", "lines"]
-        custom_header_lines = ["line_id", "user_id", "movie_id", "user_name", "line"]
         self.movies_data = pd.read_csv(
-            os.path.join("movie_dataset", "movie_conversations.tsv"),
+            os.path.join("Filtered Datasets", "filtered_movie_conversations.tsv"),
             sep="\t",
-            header=None,
-            names=custom_header,
         )  # reads the tsv movie conversations file
+
+        # convert the string of line ids to a list of line ids ["L598", "L599", "L600", "L601"]
+        self.movies_data['lines'] = self.movies_data['lines'].apply(lambda x: ast.literal_eval(x))
+
         self.lines_data = pd.read_csv(
-            os.path.join("movie_dataset", "movie_lines.tsv"),
+            os.path.join("Filtered Datasets", "filtered_movie_lines.tsv"),
             sep="\t",
-            header=None,
-            names=custom_header_lines,
-            on_bad_lines="skip",
         )
         # reads the tsv movie_lines file
 
         self.max_seq_len = max_seq_len
-        self.unk_token = self.UNK
-        self.end_token = self.END_TOKEN
-        self.start_token = self.START_TOKEN
 
         if voc_init:
             self.voc = self.train_tokenizer()
@@ -67,7 +59,7 @@ class MoviePhrasesData(data.Dataset):
     def load_tokenizer(self):
         # Load the tokenizer
         self.tokenizer = AutoTokenizer.from_pretrained(
-            os.path.join("Baseline_chatbot", "GRU_tokenizer"))
+            os.path.join("GRU_tokenizer"))
     
     def train_tokenizer(
         self, vocab_size=18000
@@ -97,11 +89,11 @@ class MoviePhrasesData(data.Dataset):
         "bos_token": "<s>",
         "eos_token": "</s>",
         "pad_token": "<pad>",
-        }
+        } # added later
         tokenizer.add_special_tokens(special_tokens_dict)
         self.tokenizer = tokenizer
 
-        self.tokenizer.save_pretrained(os.path.join("Baseline_chatbot", "GRU_tokenizer"))
+        self.tokenizer.save_pretrained(os.path.join("GRU_tokenizer"))
 
     def load_dialogue_text(self, dialogue):
         """
@@ -110,30 +102,36 @@ class MoviePhrasesData(data.Dataset):
         Outputs:
          dict: dictionary mapping a line to the next one
         """
-        list_lines_text = dialogue["lines"]
-        list_lines = list_lines_text.strip("[]").replace("'", "").split()
+        list_lines = dialogue["lines"]  # string of line ids like "[L598 L599 L600 L601]"
         movie_id = dialogue["movie"]
         lines_dict = {}
         for i in range(len(list_lines) - 1):
+            # go over the list except the last element
             #obtain line text from its id
             line_text = self.lines_data.loc[
                 (self.lines_data["line_id"] == list_lines[i])
                 & (self.lines_data["movie_id"] == movie_id)
-            ]
+            ] # dataframes
             next_line_text = self.lines_data.loc[
                 (self.lines_data["line_id"] == list_lines[i+1])
-                & (self.lines_data["movie_id"] == movie_id)]
-            lines_dict[line_text] = next_line_text
+                & (self.lines_data["movie_id"] == movie_id)] # dataframes
+            lines_dict[line_text["line"].iloc[0]] = next_line_text["line"].iloc[0]
         return lines_dict
-            
-
-
-
-
+    
     def trim_or_pad_sentence(self, tokenized_sentence):
         """
         Trims or pads a sentence to max_seq_len.
+        Tokenized_sentence is a list of token ids (integers).
+        Adds bos and eos tokens to the sentence (which tok doesn't do by default)
+        -> that's why they're trimmed at max_seq_len - 2
         """
+        if len(tokenized_sentence) > self.max_seq_len - 2:
+            format_sentence = [self.tokenizer.bos_token_id] + tokenized_sentence[: self.max_seq_len - 2] + [self.tokenizer.eos_token_id]
+        else:
+            format_sentence = [self.tokenizer.bos_token_id] + tokenized_sentence + [self.tokenizer.eos_token_id] + [self.tokenizer.pad_token_id] * (
+                self.max_seq_len - len(tokenized_sentence) - 2
+            )
+        return torch.Tensor(format_sentence)
 
     def load_dialogue(self, dialogue):
         """
@@ -144,8 +142,17 @@ class MoviePhrasesData(data.Dataset):
         so each sentences is trimmed or padded beforehand.
         Sentences also need to be tokenized.
         """
-    
-    
+        lines_dict = self.load_dialogue_text(dialogue)
+        dict_len = len(lines_dict)
+        all_inputs = torch.zeros(dict_len, self.max_seq_len, dtype=torch.long)
+        all_outputs = torch.zeros(dict_len, self.max_seq_len, dtype=torch.long)
+        for idx, (line, next_line) in enumerate(lines_dict.items()):
+            tokenized_line = self.tokenizer.encode(line)
+            tokenized_next_line = self.tokenizer.encode(next_line)
+            all_inputs[idx] = self.trim_or_pad_sentence(tokenized_line)
+            all_outputs[idx] = self.trim_or_pad_sentence(tokenized_next_line)
+        return all_inputs, all_outputs
+
     # number of dialogues, 83097
     def __len__(self):
         return len(self.movies_data)
@@ -163,6 +170,15 @@ class MoviePhrasesData(data.Dataset):
     # return the dialogue index and the tensors for (all_inputs, all_outputs) 
 
 # %%
+    
+
+"""
+My TEST:
+for i in tqdm(range(len(dataset))):
+    dataset.load_dialogue(dataset.movies_data.iloc[i])
+-> with the new dataset this gives no error
+"""
+
  """
  EXAMPLE TO ENCODE BATCHES!
  
