8d759ad7037e7175fc9069c0c9e506e652c1a793
VICTOR ABIA <transcendingvictor@gmail.com>
2024-04-02 17:47:11 +0200

    script to test the chatbot

--- Baseline_chatbot/test.py
+++ Baseline_chatbot/test.py
@@ -14,12 +14,12 @@ settings = read_settings("config.yaml")
 data_settings = settings.get('data', {})
 model_settings = settings.get('model', {})
 
+device = (torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"))
 # Load the checkpoint (specify the path accordingly)
-checkpoint = torch.load("Baseline_LSTM_ckpt_.pth", map_location=torch.device('cpu'))
+checkpoint = torch.load("model_and_optimizer.pth", map_location=device)
 
-moviephrasesdata = MoviePhrasesData(voc_init=False, max_seq_len=data_settings['max_seq'])
-voc = moviephrasesdata.vocab
-
+dataset = MoviePhrasesData(voc_init=False, max_seq_len=data_settings['max_seq'])
+voc = dataset.vocab
 
 # Initialize the models
 encoder = EncoderGRU(vocab_size=len(voc), embedding_size=model_settings['embedding_dim'], hidden_size=model_settings['hidden_dim']) 
@@ -32,31 +32,37 @@ encoder.eval()
 decoder.eval()
 
 # %% A function like this, not necessarily this.
-# def chat(input_text):
-#     # Preprocess the input_text: tokenize, convert to tensor, etc.
-#     # This heavily depends on how your data was processed during training
-#     input_tensor = ... 
+def chat(input_text: str):
+    input_tensor = dataset.tokenizer.encode(input_text)
+    input_tensor = dataset.trim_or_pad_sentence(input_tensor)
 
-#     # Encode the input
-#     with torch.no_grad():  # No need to track gradients
-#         encoder_output, encoder_hidden = encoder(input_tensor)
+    # Encode the input
+    encoder_input = input_tensor.view(-1, 1, dataset.max_seq_len)[0]
+    with torch.no_grad():  # No need to track gradients
+        encoder_output, encoder_hidden = encoder(encoder_input)
 
-#     # Create initial decoder input (typically the start-of-sequence token)
-#     decoder_input = torch.tensor([[SOS_token]], device=device)  # Adjust SOS_token accordingly
-#     decoder_hidden = encoder_hidden  # Depending on your architecture, might need adjustment
+    # Create initial decoder input (typically the start-of-sequence token)
+    decoder_input = torch.tensor(dataset.tokenizer.encode(dataset.bos_token),
+                                  device=device)  #  BOS_token
+    decoder_hidden = encoder_hidden[0] 
 
-#     decoded_words = []
-#     for di in range(max_length):  # max_length is a predefined limit to avoid infinite loops
-#         with torch.no_grad():
-#             decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
-#             topv, topi = decoder_output.data.topk(1)  # Get the most likely next word
-#             if topi.item() == EOS_token:  # Adjust EOS_token accordingly
-#                 break
-#             else:
-#                 decoded_words.append(topi.item())
+    decoded_words = []
+    for di in range(dataset.max_seq_len):  # max_length is a predefined limit to avoid infinite loops
+        with torch.no_grad():
+            # print(f"decoder_input shape is {decoder_input.shape}, decoder_hidden shape is {decoder_hidden.shape}")
+            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+            # print(f" {di} decoder_output.data shape is {decoder_output.data.shape} and decoder_hidden shape is {decoder_hidden.shape}")
+            topv, topi = decoder_output.topk(1)  # Get the most likely next word
+            #print(f"topv is {topv} and topi is {topi}")
+            if topi.item() == 18001:  # EOS_token
+                print("yup")
+                break
+            else:
+                decoded_words.append(topi.item())
 
-#             decoder_input = topi.squeeze().detach()
-
-#     # Convert the decoded sequence of IDs back to words
-#     output_sentence = ' '.join([voc.index2word[id] for id in decoded_words])
-#     return output_sentence
+            decoder_input = topi.view(1, -1)[0]
+    # add some padding to the output if necessary (not implemented yet)
+    # Convert the decoded sequence of IDs back to words
+    output_sentence = ' '.join([dataset.tokenizer.decode(id) for id in decoded_words])
+    return output_sentence
+# %%
